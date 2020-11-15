14e72a52ca8636d6970d9233ebd0af596b29cc27
Salim Hafid <salim.hafid.01@gmail.com>
2020-11-15 18:22:43 +0100

    Functional Image Pyramid + Sliding window (no classifier included yet)

--- Face_Detector/main.py
+++ Face_Detector/main.py
@@ -9,15 +9,25 @@
 # How to detect faces of larger size? Just decrease the size of the input image. You can process several sizes,
 # for example by reducing by a factor of 1.2 each time. This is called a (image) pyramid.
 
-from Face_Classifier.loader import load_single_dataset
+# Au début on a une image 500*500 par exemple
+# Boucler
+#   Parcourir chaque position (x,y) de cette image pour décider s'il y a un visage ou non
+#   S'il n'y a pas de visage
+#       Réduire l'image d'un facteur de 1.2 et recommencer
+
+from FaceRecognition.Face_Classifier.loader import load_single_dataset
 import torchvision.transforms as transforms
-from Face_Classifier.net import Net
+from FaceRecognition.Face_Classifier.net import Net
 import torch
-from utils import dataset_show
+from FaceRecognition.utils import dataset_show, imshow
 import matplotlib.pyplot as plt
+from PIL import Image
+from FaceRecognition.utils import pyramid_sliding_window, simplified_window
+import imutils
+import numpy as np
 
 # GLOBAL VARIABLES
-dataset_dir = '/home/salim/PycharmProjects/Face_Recognition/Detector_DATA/v1/BW'
+dataset_dir = '/home/salim/Coding/Face_Recognition/Datasets/Detector_DATA/v1/BW'
 transform = transforms.Compose(
         [transforms.Grayscale(),
          transforms.ToTensor(),
@@ -27,9 +37,9 @@ valid_size = 0.2
 # plus rapide des poids du net. En général la convergence est plus rapide avec
 # des batchs petits mais l'exécution est plus lente (epochs plus longues)
 # Valeurs = puissances de 2 en général, pour faciliter le travail GPU
-batch_size = 32
+batch_size = 1
 classes = ('noface','face')
-model_path = '/home/salim/PycharmProjects/Face_Recognition/Face_Classifier/model_v0.pth'
+model_path = '/home/salim/Coding/Face_Recognition/FaceRecognition/Face_Classifier/model_v0.pth'
 
 
 if __name__ == '__main__':
@@ -46,16 +56,30 @@ if __name__ == '__main__':
     net.load_state_dict(torch.load(model_path))
 
     # Apply the face classifier on each position (x,y) of the image
-    images, labels = dataset_show(data_loader,classes)
+    dataiter = iter(data_loader)
+    image, label = dataiter.next() #img.shape = [1,1,500,500]
 
+    # Loop over each 500*500 image
+    # For each image
+    #   Cut a 32*32 window and give it to the net as an input
+    #   Move the window 1 pixel to the right
+
+    # Original tensor size is [32,1,500,500] (if batch = 32)
+    # image.permute(2, 3, 1, 0) gives [500,500,32,1] -> blue/green img
+    # ou plus simplement on vire les dimensions qui ne nous intéressent pas
+    image = image[0,0,:,:]
+    pyramid_sliding_window(np.array(image), 1.2, 36, 36, 5)
+
+    '''
     outputs = net(images) #ça plante ici -> size mismatch
     _, predicted = torch.max(outputs, 1)
 
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(20)))
+    '''
 
     # Decide if the image contains a face or not (softmax ?)
 
     # Implement some visualization method to color the detected face on the picture
 
     # Little workaround to stop the matplotlib show() from blocking the console
-    plt.show()
+    #plt.show()
--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import numpy as np
 import torchvision
 import os
 from PIL import Image
+import cv2
+import time
+import imutils
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -34,5 +37,51 @@ def convert_dataset_to_bw(dataset_dir, destination_dir,img_size):
         img = img.resize(img_size)
         #Save the BW image
         img.save(destination_dir+"/group_"+str(idx)+".png")
+
+
+
+def pyramid(image, scale=1.5, minSize=(30, 30)):
+	# yield the original image
+	yield image
+	# keep looping over the pyramid
+	while True:
+		# compute the new dimensions of the image and resize it
+		w = int(image.shape[0] / scale)
+		image = imutils.resize(image, width=w)
+
+		# if the resized image does not meet the supplied minimum
+		# size, then stop constructing the pyramid
+		if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+			break
+		# yield the next image in the pyramid
+		yield image
+
+def sliding_window(image, stepSize, windowSize):
+	# slide a window across the image
+	for y in range(0, image.shape[0], stepSize):
+		for x in range(0, image.shape[1], stepSize):
+			# yield the current window
+			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+
+def pyramid_sliding_window(image, scale, winW, winH, stepSize):
+    # loop over the image pyramid
+    for resized in pyramid(image, scale=scale):
+        print("resized size = ",resized.shape)
+        # loop over the sliding window for each layer of the pyramid
+        for (x, y, window) in sliding_window(resized, stepSize=stepSize, windowSize=(winW, winH)):
+            # if the window does not meet our desired window size, ignore it
+            if window.shape[0] != winH or window.shape[1] != winW:
+                continue
+            # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+            # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+            # WINDOW
+            # since we do not have a classifier, we'll just draw the window
+
+            clone = resized.copy() #copy=np funct
+            img = cv2.rectangle(clone, (x, y), (x + winW, y + winH), (255, 0, 0), 4)
+            cv2.imshow("Window", img)
+            cv2.waitKey(1)
+            time.sleep(0.00025)#0.025
 
 
